98298b5fb421a49ca7ac5229e41700a4c29a4873
Gustavo Nicacio <gustavonicacion@gmail.com>
2024-11-05 11:00:53 -0300

    Implementar validações nos campos do formulário através da classe Validators

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class PensamentoService {
 
-  private readonly API = 'http://localhost:3000/personagem'
+  private readonly API = 'http://localhost:3000/pensamentos'
 
   constructor(private http: HttpClient) {}
 
