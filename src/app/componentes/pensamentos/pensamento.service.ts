bf186a68752c06e5e86b282cc769937c26464cd9
Gustavo Nicacio <gustavonicacion@gmail.com>
2024-11-05 13:44:27 -0300

    resolvendo conflitos2

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -6,10 +6,6 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-<<<<<<< HEAD
-=======
-
->>>>>>> development
 export class PensamentoService {
 
   private readonly API = 'http://localhost:3000/pensamentos'
@@ -36,11 +32,6 @@ export class PensamentoService {
   buscarPorId(id: string): Observable<Pensamento> {
     const URL = `${this.API}/${id}`
     return this.http.get<Pensamento>(URL)
-<<<<<<< HEAD
-
-  }
-=======
   }
 
->>>>>>> development
 }
