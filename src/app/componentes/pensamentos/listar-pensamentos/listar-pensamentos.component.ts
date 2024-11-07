a49c18ef878c15c42d9a8cac7aa19ed3d889c341
23001009 <gustavonicacion@gmail.com>
2024-11-07 09:39:08 -0300

    feat: botão para carregar mais pensamentos

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -2,19 +2,35 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 
+
 @Component({
   selector: 'app-listar-pensamentos',
   templateUrl: './listar-pensamentos.component.html',
   styleUrls: ['./listar-pensamentos.component.css']
 })
 export class ListarPensamentosComponent implements OnInit {
+  listaPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
-  listaPensamentos: Pensamento[] = []
-
-  constructor(private service:PensamentoService) { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe((lista) => this.listaPensamentos = lista)
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual)
+      .subscribe(novos => {
+        this.listaPensamentos.push(...novos);
+        console.log(this.listaPensamentos)
+        if(!novos.length) {
+          this.haMaisPensamentos = false
+        }
+      })
+
   }
 
 }
